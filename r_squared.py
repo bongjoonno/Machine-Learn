d97795f47cfc0e9a1b188d080a534e145af3a6e6
bongjoonno <zachary.feryate@gmail.com>
2025-09-02 20:11:08 -0700

    wrote r_squared function

---
+++ r_squared.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+def r_squared(y_pred, y_test):
+    sum_squared_prediction_difference = np.sum((y_test - y_pred)**2)
+    sum_squared_mean_difference = np.sum((y_test - np.mean(y_test))**2)
+        
+    R_squared = 1 - (sum_squared_prediction_difference / sum_squared_mean_difference)
+        
+    return R_squared
