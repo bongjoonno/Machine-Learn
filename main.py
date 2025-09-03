8d2f958d000fb46494988e841f633298bf5cd35f
bongjoonno <zachary.feryate@gmail.com>
2025-09-02 20:36:31 -0700

    idk

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ y = [1,2,3,4,5,6,7,8,9,10]
 
 x_train, y_train, x_test, y_test = train_test_split(x, y)
 
-y_pred = linear_regression(x_train, y_train, x_test, y_test)
+y_pred = linear_regression(x_train, y_train, x_test)
 
 r_2 = r_squared(y_pred, y_test)
 
