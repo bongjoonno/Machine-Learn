0a81e6bb5113ca0bb3580551c9ecbe610b9bb3d1
bongjoonno <zachary.feryate@gmail.com>
2025-09-03 19:33:03 -0700

    testing logistic regression

--- logistic_regression.py
+++ logistic_regression.py
@@ -14,9 +14,12 @@ def logistic_regression(x_train, y_train, x_test, epochs=1000, learning_rate=0.0
         gradient = (1/n) * (errors @ x_train)
         theta -= learning_rate * gradient
         
-    y_pred_test = x_test @ theta
+    y_pred_test = sigmoid(x_test @ theta)
     
     return y_pred_test
 
+
 def sigmoid(x):
-    return 1 / (1 + np.exp(-x))
+    return np.where(x >= 0, 
+                   1 / (1 + np.exp(-x)), 
+                   np.exp(x) / (1 + np.exp(x)))
--- main.py
+++ main.py
@@ -1,16 +1,21 @@
 from r_squared import r_squared
 from linear_regression import linear_regression
+from logistic_regression import logistic_regression
 from train_test_split import train_test_split
 
-x = [1,2,3,4,5,6,7,8,9,10]
-y = [1,2,3,4,5,6,7,8,9,10]
+x = [x for x in range(1000)]
+
+start = 0
+y = []
+
+for _ in range(1000):
+    y.append(start)
+    start += 0.001
+
+y = [1 if num >= 0.5 else 0 for num in y]
 
 x_train, y_train, x_test, y_test = train_test_split(x, y)
 
-y_pred = linear_regression(x_train, y_train, x_test)
+y_pred = logistic_regression(x_train, y_train, x_test)
 
-r_2 = r_squared(y_pred, y_test)
-
-print(r_2)
-
-
+print(y_pred)
