31783609f44859e7d784235e72649eec0c78be3e
bongjoonno <zachary.feryate@gmail.com>
2025-09-03 19:17:30 -0700

    added sigmoid function

--- logistic_regression.py
+++ logistic_regression.py
@@ -9,7 +9,7 @@ def logistic_regression(x_train, y_train, x_test, epochs=1000, learning_rate=0.0
     theta = np.random.rand(x_train.shape[1])
     
     for _ in range(epochs):
-        y_pred = x_train @ theta
+        y_pred = sigmoid(x_train @ theta)
         errors = y_pred - y_train
         gradient = (1/n) * (errors @ x_train)
         theta -= learning_rate * gradient
@@ -17,3 +17,6 @@ def logistic_regression(x_train, y_train, x_test, epochs=1000, learning_rate=0.0
     y_pred_test = x_test @ theta
     
     return y_pred_test
+
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
