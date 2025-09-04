8b670c92f6acf3b36559834a462edc485e5f07e0
bongjoonno <zachary.feryate@gmail.com>
2025-09-03 19:15:36 -0700

    copy and pasted linear_regression steps into logistic regressino script

---
+++ logistic_regression.py
@@ -0,0 +1,19 @@
+from imports import np
+
+def logistic_regression(x_train, y_train, x_test, epochs=1000, learning_rate=0.05):
+    n = len(x_train)
+    
+    x_train = np.column_stack((np.ones(len(x_train)), x_train))
+    x_test = np.column_stack((np.ones(len(x_test)), x_test))
+    
+    theta = np.random.rand(x_train.shape[1])
+    
+    for _ in range(epochs):
+        y_pred = x_train @ theta
+        errors = y_pred - y_train
+        gradient = (1/n) * (errors @ x_train)
+        theta -= learning_rate * gradient
+        
+    y_pred_test = x_test @ theta
+    
+    return y_pred_test
