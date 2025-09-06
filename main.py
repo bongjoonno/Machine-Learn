06d3cbb0f6a9bd3d91f963238b094758bce908e3
bongjoonno <zachary.feryate@gmail.com>
2025-09-06 02:32:17 +0000

    added standard scaler to imports script

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
+from imports import np, StandardScaler
+
 from r_squared import r_squared
 from linear_regression import linear_regression
 from logistic_regression import logistic_regression
 from train_test_split import train_test_split
-from imports import np
 from test_data import x, y
-from sklearn.preprocessing import StandardScaler
 from categorical_accuracy import categorical_acc
 
 x_train, y_train, x_test, y_test = train_test_split(x, y)
