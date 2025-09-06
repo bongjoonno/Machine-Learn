fe1259bdc2ff0cb306683d214ba180b265e96a53
bongjoonno <zachary.feryate@gmail.com>
2025-09-06 03:21:08 +0000

    scale added float

--- scale_data.py
+++ scale_data.py
@@ -3,7 +3,7 @@ from imports import StandardScaler
 def scale_data(x_train, x_test, columns_to_scale):
     scaler = StandardScaler()
 
-    x_train.loc[:, columns_to_scale] = scaler.fit_transform(x_train.loc[:, columns_to_scale])
+    x_train.loc[:, columns_to_scale] = scaler.fit_transform(x_train.loc[:, columns_to_scale]).astype(float)
     x_test.loc[:, columns_to_scale] = scaler.transform(x_test.loc[:, columns_to_scale])
 
     return x_train, x_test
