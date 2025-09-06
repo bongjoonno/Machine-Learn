6ae0b3924b78203b5b3d9c7052933988fdd8c957
bongjoonno <zachary.feryate@gmail.com>
2025-09-06 02:20:20 +0000

    imports breast-cancer data

--- test_data.py
+++ test_data.py
@@ -1,1 +1,9 @@
 import pandas as pd
+
+df = pd.read_csv('/workspaces/first_repo/test_data/breast-cancer.csv')
+
+df = df.drop(columns=['id'])
+df['diagnosis'] = df['diagnosis'].map({'M': 1, 'B': 0})
+
+x = df.drop(columns=['diagnosis'])
+y = df['diagnosis']
