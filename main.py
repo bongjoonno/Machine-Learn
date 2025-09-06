8a0bc2ed64d03c11b6946dc7d59e242495af1cd2
bongjoonno <zachary.feryate@gmail.com>
2025-09-06 02:30:46 +0000

    completed categorical test scripts and categorical accuracy

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from train_test_split import train_test_split
 from imports import np
 from test_data import x, y
 from sklearn.preprocessing import StandardScaler
+from categorical_accuracy import categorical_acc
 
 x_train, y_train, x_test, y_test = train_test_split(x, y)
 
@@ -13,4 +14,6 @@ scaler = StandardScaler()
 x_train = scaler.fit_transform(x_train)
 x_test = scaler.transform(x_test)
 
-print(x_test)
+y_pred = logistic_regression(x_train, y_train, x_test)
+
+print(categorical_acc(y_pred, y_test))
