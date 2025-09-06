1ded469a9bdf8c5d8d03db3ed5a5291f5d6cf588
bongjoonno <zachary.feryate@gmail.com>
2025-09-06 03:01:48 +0000

    completed scale data script

--- scale_data.py
+++ scale_data.py
@@ -1,2 +1,9 @@
+from imports import StandardScaler
+
 def scale_data(x_train, x_test, columns_to_scale):
-    
+    scaler = StandardScaler()
+
+    x_train[columns_to_scale] = scaler.fit_transform(x_train.loc[:, columns_to_scale])
+    x_test[columns_to_scale] = scaler.transform(x_test.loc[:, columns_to_scale])
+
+    return x_train, x_test
