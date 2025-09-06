0cb05696e5b5578feecde6b4704e24faca85c962
bongjoonno <zachary.feryate@gmail.com>
2025-09-06 02:50:12 +0000

    finished continuous data preparation

--- continuous_test_data.py
+++ continuous_test_data.py
@@ -3,4 +3,11 @@ from imports import pd
 df = pd.read_csv('/workspaces/first_repo/test_data/insurance.csv')
 
 df['sex'] = df['sex'].map({'female' : 0, 'male' : 1})
-print(df)
+df['smoker'] = df['smoker'].map({'yes' : 1, 'no' : 0})
+
+regions = pd.get_dummies(df['region'], dtype=int)
+
+df = pd.concat([df, regions], axis=1)
+
+x_continuous = df.drop(columns='charges')
+y_continuous = df['charges']
