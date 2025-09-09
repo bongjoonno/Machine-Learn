bb68387412e973fb0d4a188a36e89a92ef376543
bongjoonno <zachary.feryate@gmail.com>
2025-09-09 00:21:43 -0700

    added main_testing_imports script so main is less clunky

---
+++ main_testing_imports.py
@@ -0,0 +1,13 @@
+from imports import np, pd, StandardScaler
+
+from r_squared import r_squared
+from linear_regression import linear_regression
+from logistic_regression import logistic_regression
+from train_test_split import train_test_split
+from categorical_accuracy import categorical_acc
+
+from breast_cancer_data_prep import breast_cancer_x, breast_cancer_y
+from insurance_costs_data_prep import insurance_x, insurance_y
+from email_spam_data_prep import email_ham, email_spam
+
+from scale_data import scale_data
