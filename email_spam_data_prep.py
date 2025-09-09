fbd6fbfd577d6db5c556b4baf17ec465977b0ba5
bongjoonno <zachary.feryate@gmail.com>
2025-09-09 00:26:40 -0700

    interesting....

--- email_spam_data_prep.py
+++ email_spam_data_prep.py
@@ -1,6 +1,7 @@
 from imports import pd
 
-email_df = pd.read_csv('/content/drive/MyDrive/Colab Notebooks/data/email_spam.csv')
+email_df = pd.read_csv('/workspaces/first_repo/test_data/email_spam.csv')
+
 email_df = email_df[email_df['Category'].isin(['ham', 'spam'])]
 
 email_ham = email_df[email_df['Category'] == 'ham']
--- main.py
+++ main.py
@@ -1,1 +1,3 @@
 from main_testing_imports import *
+
+print(breast_cancer_x)
