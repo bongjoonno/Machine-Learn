4ee7d2bf141553484c8af0297b738c328a92226d
bongjoonno <zachary.feryate@gmail.com>
2025-09-11 00:47:46 +0000

    converted linear regression to OOP

--- linear_regression.py
+++ linear_regression.py
@@ -1,21 +1,33 @@
 from imports import np
 
-def linear_regression(x_train, y_train, x_test, epochs=1000, learning_rate=0.05):
-    n = len(x_train)
-    
-    x_train = np.column_stack((np.ones(len(x_train)), x_train))
-    x_test = np.column_stack((np.ones(len(x_test)), x_test))
-    
-    theta = np.random.rand(x_train.shape[1])
-    
-    one_divided_by_n = 1/n
-    
-    for _ in range(epochs):
-        y_pred = x_train @ theta
-        errors = y_pred - y_train
-        gradient = one_divided_by_n * (x_train.T @ errors)
-        theta -= learning_rate * gradient
+class LinearRegression:
+    def __init__(self, x_train, y_train, x_test, y_test):
+        self.x_train = x_train
+        self.y_train = y_train
+        self.x_test = x_test
+        self.y_test = y_test
+        self.theta = np.random.rand(self.x_train.shape[1])
+
+    def train(self, epochs=1000, learning_rate=0.05):
+        n = len(self.x_train)
         
-    y_pred_test = x_test @ theta
+        self.x_train = np.column_stack((np.ones(len(self.x_train)), self.x_train))
+        self.x_test = np.column_stack((np.ones(len(self.x_test)), self.x_test))
+        
+        one_divided_by_n = 1/n
+        
+        for _ in range(epochs):
+            y_pred = self.x_train @ self.theta
+            errors = y_pred - self.y_train
+            gradient = one_divided_by_n * (self.x_train.T @ errors)
+            self.theta -= learning_rate * gradient
     
-    return y_pred_test
+    def test(self):
+        y_pred = self.x_test @ self.theta
+        return sum(y_pred == self.y_test)
+            
+        
+
+
+#LinearRegression() Object
+#train and test methods
