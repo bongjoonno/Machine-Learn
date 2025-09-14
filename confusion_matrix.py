2af0b8c4384bd5e95167374b18ecfdb3d3d8bcc7
bongjoonno <zachary.feryate@gmail.com>
2025-09-14 09:15:10 +0000

    started on confusion matrix func

--- confusion_matrix.py
+++ confusion_matrix.py
@@ -1,1 +1,15 @@
-from imports import np
+from imports import np, pd
+from naive_bayes_testing import email_df, class_labels
+
+def create_confusion_matrix(y_test, y_pred):
+    unique_classes = pd.unique(y_test)
+    
+    class_mapping = {unique_classes[i]: i for i in range(len(unique_classes))}
+
+    y_test = y_test.map(class_mapping)
+    y_pred = y_pred.map(class_mapping)
+
+    return y_test, y_pred
+
+res = create_confusion_matrix(email_df['Prediction'], email_df['Category'])
+print(res)
