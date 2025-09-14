71b48f72cefb999a52996a62acc87ba09734a95c
bongjoonno <zachary.feryate@gmail.com>
2025-09-14 10:07:02 +0000

    finished confusion matrix! (almostt)

--- confusion_matrix.py
+++ confusion_matrix.py
@@ -1,15 +1,22 @@
 from imports import np, pd
-from naive_bayes_testing import email_df, class_labels
+from logistic_regression_test import breast_cancer_y_test, y_pred
 
 def create_confusion_matrix(y_test, y_pred):
-    unique_classes = pd.unique(y_test)
-    
-    class_mapping = {unique_classes[i]: i for i in range(len(unique_classes))}
+    y_test = pd.Series(y_test)
+    y_pred = pd.Series(y_pred)
 
-    y_test = y_test.map(class_mapping)
-    y_pred = y_pred.map(class_mapping)
+    y_test_value_counts = y_test.value_counts()
+    y_pred_value_counts = y_pred.value_counts()
 
-    return y_test, y_pred
+    errors = pd.DataFrame(y_test_value_counts - y_pred_value_counts).T
 
-res = create_confusion_matrix(email_df['Prediction'], email_df['Category'])
+    positives = pd.DataFrame(y_test_value_counts + y_pred_value_counts).T
+
+    positives = positives - errors
+    errors = abs(errors)
+
+    res = pd.concat([positives, errors])
+    return res
+
+res = create_confusion_matrix(breast_cancer_y_test, y_pred)
 print(res)
