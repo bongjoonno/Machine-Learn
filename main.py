216be3cc84515d7e9dabcef9a9a3ad631ba5b52d
zachary <zachary.feryate@gmail.com>
2025-11-03 00:29:58 -0800

    it works, just had to call from main

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
-from models.linear_regression import LinearRegression
+from model_tests.linear_regression_testing import linear_regression_test
 
-x = LinearRegression()
+
+
+
+
+if __name__ == '__main__':
+    print(linear_regression_test())
--- model_tests/linear_regression_testing.py
+++ model_tests/linear_regression_testing.py
@@ -3,14 +3,15 @@ from data_imports.data_imports import insurance_x, insurance_y
 from data_manipulation.data_manipulation_imports import train_test_split, scale_data
 from metrics.metrics_imports import r_squared
 
-insurance_x_train, insurance_y_train, insurance_x_test, insurance_y_test = train_test_split(insurance_x, insurance_y)
+def linear_regression_test():
+    insurance_x_train, insurance_y_train, insurance_x_test, insurance_y_test = train_test_split(insurance_x, insurance_y)
 
-insurance_x_train, insurance_x_test = scale_data(insurance_x_train, insurance_x_test, ['age', 'bmi', 'children'])
+    insurance_x_train, insurance_x_test = scale_data(insurance_x_train, insurance_x_test, ['age', 'bmi', 'children'])
 
-linear_regression_model = LinearRegression()
+    linear_regression_model = LinearRegression()
 
-linear_regression_model.train(insurance_x_train, insurance_y_train)
-y_pred = linear_regression_model.predict(insurance_x_test)
+    linear_regression_model.train(insurance_x_train, insurance_y_train)
+    y_pred = linear_regression_model.predict(insurance_x_test)
 
-linear_regression_r_squared = r_squared(y_pred, insurance_y_test)
-print(linear_regression_r_squared)
+    linear_regression_r_squared = r_squared(y_pred, insurance_y_test)
+    return linear_regression_r_squared
