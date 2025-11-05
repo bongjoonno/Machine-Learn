ba647b2197b0d95ac1df18728d411febc322a035
zachary <zachary.feryate@gmail.com>
2025-11-05 00:49:05 -0800

    scaling data manually now

--- tests/ga_hyperparameter_optimizer_test.py
+++ tests/ga_hyperparameter_optimizer_test.py
@@ -2,19 +2,23 @@ from machine_learn.data_manipulation import train_test_validate_split
 from src.machine_learn.models.linear_regression import LinearRegression
 from src.machine_learn.data_imports.data_imports import insurance_x, insurance_y
 from src.machine_learn.data_manipulation.train_test_validate_split import train_test_validate_split
-from src.machine_learn.data_manipulation.scale_data import scale_data
 from src.machine_learn.metrics.metrics_imports import r_squared
 from src.machine_learn.genetic_algorithms.ga_hyperparameter_optimizer import GAHParamOptimizer
+from src.machine_learn.imports import StandardScaler
 
 def ga_hyperparameter_optimizer_test():
     x_train, y_train, x_test, y_test, x_val, y_val = train_test_validate_split(insurance_x, insurance_y)
+    scaler = StandardScaler()
+    scaler.fit(x_train)
 
-    insurance_x_train, insurance_x_test = scale_data(insurance_x_train, insurance_x_test, ['age', 'bmi', 'children'])
+    x_train.loc[:, ['age', 'bmi', 'children']] = scaler.transform(x_train.loc[:, ['age', 'bmi', 'children']])
+    x_test.loc[:, ['age', 'bmi', 'children']] = scaler.transform(x_test.loc[:, ['age', 'bmi', 'children']])
+    x_val.loc[:, ['age', 'bmi', 'children']] = scaler.transform(x_val.loc[:, ['age', 'bmi', 'children']])
 
     linear_regression_model = LinearRegression()
     ga_hparameter_optimizer = GAHParamOptimizer()
     
-    ga_hparameter_optimizer.optimize(linear_regression_model=, x_validation, y_validation)
+    ga_hparameter_optimizer.optimize(linear_regression_model, x_val, y_val)
     
 
 
