0555c0f5f93e61a70ba145a896490214f52105a8
zachary <zachary.feryate@gmail.com>
2025-11-05 00:25:56 -0800

    added mse and min_mse to linear_regression model

--- src/machine_learn/models/linear_regression.py
+++ src/machine_learn/models/linear_regression.py
@@ -8,12 +8,17 @@ class LinearRegression:
         self.one_divided_by_n = 1/(len(self.x))
 
         self.theta = np.zeros(self.x.shape[1])
-        
+        self.min_loss = float('inf')
+
         for _ in range(epochs):
             y_pred = self.x @ self.theta
             errors = y_pred - self.y
             gradient = self.one_divided_by_n * (self.x.T @ errors)
             self.theta -= learning_rate * gradient
+
+            mse = np.mean(errors**2)
+            self.min_loss = min(mse, self.min_loss)
+
     
     def predict(self, x_test):
         x_test = np.column_stack((np.ones(len(x_test)), x_test))
