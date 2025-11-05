761c6ffdf4cdbf39a5ad2a597eb0a041a429a3bd
zachary <zachary.feryate@gmail.com>
2025-11-04 23:31:54 -0800

    made basic population generation function

--- src/machine_learn/genetic_algorithms/ga_hyperparameter_optimizer.py
+++ src/machine_learn/genetic_algorithms/ga_hyperparameter_optimizer.py
@@ -1,6 +1,6 @@
+from src.machine_learn.imports import np
 
-
-class GeneticHParamOptimizer:
+class GAHParamOptimizer:
     epoch_low = 1
     epoch_high = 100_000
 
@@ -11,9 +11,12 @@ class GeneticHParamOptimizer:
         population = []
 
         for _ in range(size):
-            pass
+            random_epochs = np.random.randint(GAHParamOptimizer.epoch_low, GAHParamOptimizer.epoch_high)
+            random_learning_rate = np.random.uniform(GAHParamOptimizer.learning_rate_low, GAHParamOptimizer.learning_rate_high)
+
+            population.append((random_epochs, random_learning_rate))
         
-        pass
+        return population
 
 
 
