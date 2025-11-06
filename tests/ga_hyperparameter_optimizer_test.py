a238839d2b809e64c4b50c6c9c7e83e41a53b47e
bongjoonno <zachary.feryate@gmail.com>
2025-11-05 22:01:58 -0800

    added min_loss attribute to logistic regression class

--- tests/ga_hyperparameter_optimizer_test.py
+++ tests/ga_hyperparameter_optimizer_test.py
@@ -2,13 +2,12 @@ from src.machine_learn.data_manipulation import train_test_validate_split
 from src.machine_learn.models.logistic_regression import LogisticRegression
 from src.machine_learn.data_imports.breast_cancer_data import breast_cancer_x, breast_cancer_y
 from src.machine_learn.data_manipulation.train_test_validate_split import train_test_validate_split
-from src.machine_learn.data_manipulation.train_test_split import train_test_split
 from src.machine_learn.genetic_algorithms.ga_hyperparameter_optimizer import GAHParamOptimizer
 from src.machine_learn.imports import StandardScaler
 from src.machine_learn.metrics.categorical_accuracy import categorical_accuracy
 
 def ga_hyperparameter_optimizer_test():
-    x_train, y_train, x_test, y_test = train_test_split(breast_cancer_x, breast_cancer_y)
+    x_train, y_train, x_test, y_test, x_val, y_val = train_test_validate_split(breast_cancer_x, breast_cancer_y)
     scaler = StandardScaler()
     
     x_train = scaler.fit_transform(x_train)
@@ -18,14 +17,10 @@ def ga_hyperparameter_optimizer_test():
 
 
     logistic_regression_model = LogisticRegression()
-    #ga_hparameter_optimizer = GAHParamOptimizer()
+    ga_hparameter_optimizer = GAHParamOptimizer()
     
-    #ga_hparameter_optimizer.optimize(linear_regression_model, x_val, y_val)
-    
+    ga_hparameter_optimizer.optimize(logistic_regression_model, x_val, y_val)
 
-    logistic_regression_model.train(x_train, y_train)
-    y_pred = logistic_regression_model.predict(x_test)
-    return categorical_accuracy(y_pred, y_test)
 
 # generate initial population of random (epoch, learning_rate) tuple pairs
 # random epochs in range (1, 100_000) and learning_rate from (0.000001, 0.5)
