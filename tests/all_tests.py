130e5ecfa8c26634dbb790d67cbd3bb6fde5959f
bongjoonno <zachary.feryate@gmail.com>
2025-11-05 22:00:37 -0800

    converted ga optim test to logistic regression

--- tests/all_tests.py
+++ tests/all_tests.py
@@ -1,5 +1,4 @@
-from tests.logistic_regression_test import logistic_regression_test
-#from tests.ga_hyperparameter_optimizer_test import ga_hyperparameter_optimizer_test
+from tests.ga_hyperparameter_optimizer_test import ga_hyperparameter_optimizer_test
 
 def run_all_tests():
-    return logistic_regression_test()
+    return ga_hyperparameter_optimizer_test()
