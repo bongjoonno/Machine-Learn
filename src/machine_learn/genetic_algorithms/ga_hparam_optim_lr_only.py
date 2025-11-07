700938b8f14340928d41e190feef489ab0c032a7
zachary <zachary.feryate@gmail.com>
2025-11-07 02:58:06 -0800

    changed from multi-run single run

--- src/machine_learn/genetic_algorithms/ga_hparam_optim_lr_only.py
+++ src/machine_learn/genetic_algorithms/ga_hparam_optim_lr_only.py
@@ -3,44 +3,43 @@ from src.machine_learn.imports import np, tqdm
 class GAHParamOptim:
     learning_rate_low = 0.0001
     learning_rate_high = 0.1
-    optimization_runs = 3
 
-    def __init__(self, population_size = 20):
+    def __init__(self, population_size = 40):
         self.population_size = population_size
         self.population = [0 for _ in range(self.population_size)]
         self.fitness_scores = [0 for _ in range(self.population_size)]
 
-    def optimize(self, model, x_validation, y_validation, generations = 20):
-        best_fitness_solution_pairs = []
+    def optimize(self, model, x_validation, y_validation, generations = 5):
+        lowest_loss = float('inf')
+        lowest_loss_solution = None
 
         self.model = model
         self.x_validation = x_validation
         self.y_validation = y_validation
         self.avg_fitness_scores_per_generation = []
+        self.generate_population()
 
-        for _ in tqdm(range(GAHParamOptim.optimization_runs)):
-            self.generate_population()
+        for i in range(generations):
+            self.fitness()
 
-            for i in range(generations):
-                self.fitness()
+            generation_average_fitness_score = np.mean(self.fitness_scores)
 
-                generation_average_fitness_score = np.mean(self.fitness_scores)
-    
-                self.avg_fitness_scores_per_generation.append(generation_average_fitness_score)
+            self.avg_fitness_scores_per_generation.append(generation_average_fitness_score)
 
-                # biasin toward smaller learning rates, needs fix
-                fitness_to_population = list(zip(self.fitness_scores, self.population))
-                np.random.shuffle(fitness_to_population)
-                fitness_to_population_sorted = sorted(fitness_to_population, key=lambda x: x[0])[:self.population_size//2]
-                top_50_percent = [solution for _, solution in fitness_to_population_sorted]
+            # biasin toward smaller learning rates, needs fix
+            fitness_to_population = list(zip(self.fitness_scores, self.population))
+            np.random.shuffle(fitness_to_population)
+            fitness_to_population_sorted = sorted(fitness_to_population, key=lambda x: x[0])[:self.population_size//2]
+            top_50_percent = [solution for _, solution in fitness_to_population_sorted]
 
-                children = GAHParamOptim.make_offspring(top_50_percent)
-                self.population = top_50_percent + children
+            children = GAHParamOptim.make_offspring(top_50_percent)
+            self.population = top_50_percent + children
 
-                best_fitness_solution_pairs.append(fitness_to_population_sorted[0])
+            if ba:
+                lowest_loss = fitness_to_population_sorted[0]
+                lowest_loss_solution = fitness_to_population_sorted[1]
 
-        np.random.shuffle(best_fitness_solution_pairs)
-        return min(best_fitness_solution_pairs, key=lambda x: x[0])[1]
+        return lowest_loss_solution
             
 
 
@@ -65,8 +64,8 @@ class GAHParamOptim:
     
     @staticmethod
     def crossover(parent_a, parent_b):
-        lr_weight1 = np.random.uniform(0, 1)
-        lr_weight2 = 1 - lr_weight1
+        lr_weight1 = np.random.uniform(-0.25, 1.25)
+        lr_weight2 = 1.25 - lr_weight1
 
         child_a_lr = (parent_a*lr_weight1) + (parent_b*lr_weight2)
         child_b_lr = (parent_a*lr_weight2) + (parent_b*lr_weight1)
