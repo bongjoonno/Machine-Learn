0c67b94bb24f0d83ee5ce214eb3edd75a9f92b05
bongjoonno <zachary.feryate@gmail.com>
2025-11-07 06:24:23 -0800

    finished epochs optimizer as well

--- main.py
+++ main.py
@@ -7,9 +7,3 @@ def main():
 
 if __name__ == '__main__':
     print(main())
-    
-
-# to-do
-# go through certain number of epochs (every 10) -> 5_000
-# optimize lr for each (lr optimizer is complete)
-# pick one with lowest loss
