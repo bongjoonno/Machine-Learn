6b97eb2a6b42d418b61564806d888ad4d4ab5fbb
bongjoonno <zachary.feryate@gmail.com>
2025-11-08 05:00:06 -0800

    finished type hinting for main

--- tests/main_model_tests.py
+++ tests/main_model_tests.py
@@ -1,8 +1,9 @@
 from tests.linear_regression_test import linear_regression_test
 from tests.logistic_regression_test import logistic_regression_test
 from tests.naive_bayes_test import naive_bayes_test
+from src.machine_learn.imports import np
 
 tests = [linear_regression_test, logistic_regression_test, naive_bayes_test]
 
-def main_model_tests():
+def main_model_tests() -> list[float, float, float]:
     return [func() for func in tests]
