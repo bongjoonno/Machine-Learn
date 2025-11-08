4a0d32aa139ced568636d4c39be62b6dfa78b96e
zachary <zachary.feryate@gmail.com>
2025-11-07 18:35:48 -0800

    running tests

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from tests.main_model_tests import main_model_tests
 from tests.ga_hyperparameter_optimizer_test import ga_hyperparameter_optimizer_test
 
 def main():
-    return main_model_tests()
+    return ga_hyperparameter_optimizer_test()
 
 
 
