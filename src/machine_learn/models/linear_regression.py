99aadb907db119bce2961b4a5d6992def09bb06c
bongjoonno <zachary.feryate@gmail.com>
2025-11-08 05:54:38 -0800

    finished mypy lr

--- src/machine_learn/models/linear_regression.py
+++ src/machine_learn/models/linear_regression.py
@@ -1,9 +1,9 @@
 from src.machine_learn.imports import np
-from src.machine_learn.types import DF, NDArray
+from src.machine_learn.types import DF, Series, NDArray
 from src.machine_learn.constants import EPOCHS, LEARNING_RATE
 
 class LinearRegression:
-    def train(self, x: DF, y: DF, epochs: int = EPOCHS, learning_rate: float = LEARNING_RATE) -> None:   
+    def train(self, x: DF, y: Series, epochs: int = EPOCHS, learning_rate: float = LEARNING_RATE) -> None:   
         X = np.column_stack((np.ones(len(x)), x))
         one_divided_by_n = 1/(X.shape[1])
 
--- tests/linear_regression_test.py
+++ tests/linear_regression_test.py
@@ -4,7 +4,7 @@ from src.machine_learn.data_prep import insurance_x, insurance_y
 from src.machine_learn.data_manipulation import train_test_split, scale_data
 from src.machine_learn.metrics import r_squared
 
-def test_linear_regression() -> np.float64:
+def test_linear_regression() -> float:
     insurance_x_train, insurance_y_train, insurance_x_test, insurance_y_test = train_test_split(insurance_x, insurance_y)
 
     insurance_x_train, insurance_x_test = scale_data(insurance_x_train, insurance_x_test, columns_to_scale = ['age', 'bmi', 'children'])
