8b3b9b67af7cdf9d9a61fa81141c718347a3ccf2
bongjoonno <zachary.feryate@gmail.com>
2025-11-08 04:21:40 -0800

    making type hinting for linear regression, seeing what data type predict returns

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from tests.main_model_tests import main_model_tests
 from tests.ga_hyperparameter_optimizer_test import ga_hyperparameter_optimizer_test
 
 def main():
-    return ga_hyperparameter_optimizer_test()
+    return main_model_tests()
 
 
 
--- src/machine_learn/models/linear_regression.py
+++ src/machine_learn/models/linear_regression.py
@@ -1,8 +1,9 @@
 from src.machine_learn.imports import np
+from src.machine_learn.types import DF
 from src.machine_learn.constants import EPOCHS, LEARNING_RATE
 
 class LinearRegression:
-    def train(self, x, y, epochs=EPOCHS, learning_rate=LEARNING_RATE):   
+    def train(self, x: DF, y: DF, epochs: int = EPOCHS, learning_rate: float = LEARNING_RATE) -> None:   
         x = np.column_stack((np.ones(len(x)), x))
         one_divided_by_n = 1/(x.shape[1])
 
@@ -19,7 +20,7 @@ class LinearRegression:
             self.min_loss = min(mse, self.min_loss)
 
     
-    def predict(self, x_test):
+    def predict(self, x_test: DF):
         x_test = np.column_stack((np.ones(len(x_test)), x_test))
         y_pred = x_test @ self.theta
         return y_pred
