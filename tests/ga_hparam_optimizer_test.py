4c5a46c827018d91760c62edcfdad9642b38323d
bongjoonno <zachary.feryate@gmail.com>
2025-11-10 11:49:34 -0800

    added titanic data to ga_hparam_optimizer

--- tests/ga_hparam_optimizer_test.py
+++ tests/ga_hparam_optimizer_test.py
@@ -2,14 +2,14 @@ from src.machine_learn.imports import plt
 from src.machine_learn.constants import LEARNING_RATE
 
 from src.machine_learn.models import LogisticRegression
-from src.machine_learn.data_prep import breast_cancer_x, breast_cancer_y
+from src.machine_learn.data_prep import breast_cancer_x, breast_cancer_y, titanic_x, titanic_y
 
 from src.machine_learn.data_manipulation import train_test_validate_split, scale_data
 from src.machine_learn.metrics import categorical_accuracy
 
 from src.machine_learn.genetic_algorithms import GAlrOptimizer
 
-data = [(breast_cancer_x, breast_cancer_y), ]
+data = [(breast_cancer_x, breast_cancer_y), (titanic_x, titanic_y)]
 def test_ga_hparam_optimizer() -> None:
     for x, y in data:
         x_train, y_train, x_test, y_test, x_val, y_val = train_test_validate_split(x, y)
