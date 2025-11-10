bccac5c247dfddc720c56b7e7a9e0ad4550e40b4
bongjoonno <zachary.feryate@gmail.com>
2025-11-10 11:37:03 -0800

    made titanic data imporst

--- src/machine_learn/data_prep/titanic_data.py
+++ src/machine_learn/data_prep/titanic_data.py
@@ -1,5 +1,5 @@
-from imports import pd
-from constants import DATA_DIRECTORY
+from src.machine_learn.imports import pd
+from src.machine_learn.constants import DATA_DIRECTORY
 
 titanic_x = pd.read_csv(DATA_DIRECTORY / 'titanic_train.csv')
 titanic_y = pd.read_csv(DATA_DIRECTORY / 'titanic_test.csv')
