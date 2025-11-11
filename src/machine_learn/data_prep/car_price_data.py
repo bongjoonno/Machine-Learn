c97597ee681341a799a6584c554681a135fd7e2c
bongjoonno <zachary.feryate@gmail.com>
2025-11-10 16:21:19 -0800

    finished importing and massaging car data

--- src/machine_learn/data_prep/car_price_data.py
+++ src/machine_learn/data_prep/car_price_data.py
@@ -2,4 +2,15 @@ from src.machine_learn.imports import pd
 from src.machine_learn.constants import DATA_DIRECTORY
 
 car_price_df = pd.read_csv(DATA_DIRECTORY / 'car_price.csv')
-print(car_price_df)
+car_price_df = car_price_df.drop(columns = ['car_ID', 'CarName'])
+
+binary_columns = ['fueltype', 'aspiration', 'doornumber', 'enginelocation']
+multi_columns = ['carbody', 'drivewheel', 'enginetype', 'cylindernumber', 'fuelsystem']
+
+for col in binary_columns:
+    car_price_df[col] = car_price_df[col].astype('category').cat.codes
+
+car_price_df = pd.get_dummies(car_price_df, columns = multi_columns, dtype = int)
+
+car_price_x = car_price_df.drop(columns = ['price'])
+car_price_y = car_price_df['price']
