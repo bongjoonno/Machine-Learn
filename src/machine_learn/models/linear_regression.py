bc3a1ff03f1233d232549476ecaed03f71880e4f
bongjoonno <zachary.feryate@gmail.com>
2025-11-13 11:19:01 -0800

    added _val, yval

--- src/machine_learn/models/linear_regression.py
+++ src/machine_learn/models/linear_regression.py
@@ -4,21 +4,25 @@ from src.machine_learn.constants import EPOCHS, LEARNING_RATE
 from src.machine_learn.metrics import mean_squared_error
 
 class LinearRegression:
-    def train(self, x: DF, y: Series, epochs: int = EPOCHS, learning_rate: float = LEARNING_RATE) -> None:   
-        X = np.column_stack((np.ones(len(x)), x))
+    def train(self, x_train: DF, y_train: Series, x_val: DF, y_val: Series, epochs: int = EPOCHS, learning_rate: float = LEARNING_RATE) -> None:   
+        X = np.column_stack((np.ones(len(x_train)), x_train))
         one_divided_by_n = 1/(X.shape[1])
 
         self.theta = np.ones(X.shape[1])
-        self.min_loss = float('inf')
+        self.min_train_loss = float('inf')
 
         for _ in range(epochs):
             y_pred = X @ self.theta
-            errors = y_pred - y
+            errors = y_pred - y_train
             gradient = one_divided_by_n * (X.T @ errors)
             self.theta -= learning_rate * gradient
 
-            mse = mean_squared_error(y_pred, y)
-            self.min_loss = min(mse, self.min_loss)
+            train_mse = mean_squared_error(y_pred, y_train)
+            self.min_train_loss = min(train_mse, self.min_train_loss)
+            
+            
+            
+            
 
     
     def predict(self, x: DF) -> NDArray:
