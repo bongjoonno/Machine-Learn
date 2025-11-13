1c6e9e4d32585e1afa0cdf44f136ee5806ed2e25
bongjoonno <zachary.feryate@gmail.com>
2025-11-13 12:26:50 -0800

    including cols to scale in all data presentations

--- tests/ga_hparam_optimizer_test.py
+++ tests/ga_hparam_optimizer_test.py
@@ -2,22 +2,22 @@ from src.machine_learn.imports import plt
 from src.machine_learn.constants import LEARNING_RATE
 
 from src.machine_learn.models import LinearRegression
-from src.machine_learn.data_prep import student_x, student_y, car_price_x, car_price_y
+from src.machine_learn.data_prep import student_x, student_y, student_cols_to_scale, car_price_x, car_price_y, car_price_cols_to_scale
 
 from src.machine_learn.data_manipulation import train_test_validate_split, scale_data
 from src.machine_learn.metrics import r_squared
 
 from src.machine_learn.genetic_algorithms import GAHParamOptimizer
 
-data = [(student_x, student_y), (car_price_x, car_price_y)]
+data = [(student_x, student_y, student_x.columns), (car_price_x, car_price_y, car_price_cols_to_scale)]
 
 def test_ga_hparam_optimizer() -> None:
     
     linear_regression_model = LinearRegression()
         
-    for x, y in data:
+    for x, y, cols_to_scale in data:
         x_train, y_train, x_val, y_val, x_test, y_test = train_test_validate_split(x, y)
-        x_train, x_val, x_test = scale_data(x_train, x_val, x_test, columns_to_scale = x_train.columns)
+        x_train, x_val, x_test = scale_data(x_train, x_val, x_test, columns_to_scale = cols_to_scale)
         
         ga_hparameter_optimizer = GAHParamOptimizer(linear_regression_model, x_train, y_train, x_val, y_val)
             
