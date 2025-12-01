2d994afaad4d022ed84581e6bc12c9ea9a84b7eb
bongjoonno <zachary.feryate@gmail.com>
2025-12-01 11:55:47 -0800

    data is importted into test_ga_param_optimizer func

--- tests/__init__.py
+++ tests/__init__.py
@@ -2,4 +2,5 @@ from .linear_regression_test import test_linear_regression
 from .logistic_regression_test import test_logistic_regression
 from .naive_bayes_test import test_naive_bayes
 from .ga_hparam_optimizer_test import test_ga_hparam_optimizer
+from.ga_param_optimizer_test import test_ga_param_optimizer
 from .all_model_tests import test_all_models
--- tests/ga_param_optimizer_test.py
+++ tests/ga_param_optimizer_test.py
@@ -1,7 +1,7 @@
 from src.machine_learn.data_prep import salary_x, salary_y
 
+def test_ga_param_optimizer():
+    print(salary_x)
 
-print(salary_x)
 
-
-print(salary_y)
+    print(salary_y)
