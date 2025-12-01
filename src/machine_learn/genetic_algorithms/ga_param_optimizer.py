f026b61a55e48db8d3dccf3f075fa25048df4613
bongjoonno <zachary.feryate@gmail.com>
2025-12-01 11:54:10 -0800

    made optimizer class and printing salary data

---
+++ src/machine_learn/genetic_algorithms/ga_param_optimizer.py
@@ -0,0 +1,5 @@
+
+class GAParamOptimizer:
+    def __init__(self):
+        pass
+
---
+++ tests/ga_param_optimizer_test.py
@@ -0,0 +1,7 @@
+from src.machine_learn.data_prep import salary_x, salary_y
+
+
+print(salary_x)
+
+
+print(salary_y)
