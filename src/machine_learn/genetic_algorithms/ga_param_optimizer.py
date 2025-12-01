d83d4968ef4e01a6e4c09483b7c4be92cbd8dc4e
bongjoonno <zachary.feryate@gmail.com>
2025-12-01 12:03:58 -0800

    wrote basic population generation and trainign loop

--- src/machine_learn/genetic_algorithms/ga_param_optimizer.py
+++ src/machine_learn/genetic_algorithms/ga_param_optimizer.py
@@ -1,5 +1,26 @@
+from src.machine_learn.imports import np
+from src.machine_learn.metrics import mean_squared_error
+# still need to determine how to determine bounds
+param_lower_bound = -1000
+param_upper_bound = 1000
+population_size = 500
 
-class GAParamOptimizer:
-    def __init__(self):
-        pass
-
+def optimize_parameter(x, y):
+    '''
+    1. generate random solutions
+    2. select top 50%
+    3. generate children from top 50%
+    4. combine children and parent to make new population
+    5. repeat 2-4 until convergence
+    '''
+    
+    population = [np.random.randint(param_lower_bound, param_upper_bound) for _ in range(population_size)]
+    
+    # measure loss
+    losses = []
+    
+    for solution in population:
+        y_pred = solution @ x
+        mse = mean_squared_error(y_pred, y)
+        losses.append(mse)
+    
--- tests/ga_param_optimizer_test.py
+++ tests/ga_param_optimizer_test.py
@@ -1,7 +1,4 @@
 from src.machine_learn.data_prep import salary_x, salary_y
 
 def test_ga_param_optimizer():
-    print(salary_x)
-
-
-    print(salary_y)
+    pass
