1f9abe18e4e42a3bbc6731a2ee13d3494431ca5a
bongjoonno <zachary.feryate@gmail.com>
2025-12-05 15:24:25 -0800

    changed return type of train-tst_spit to numpy

--- src/machine_learn/genetic_algorithms/ga_param_optimizer.py
+++ src/machine_learn/genetic_algorithms/ga_param_optimizer.py
@@ -10,23 +10,12 @@ population_size = 1000
 generations = 1000
 
 def optimize_weight_and_bias_seperately(x, y):
-    x = x.to_numpy().flatten()
-    y = y.to_numpy()
-    
     n = len(x)
-    
-    '''
-    1. generate random solutions
-    2. select top 50%
-    3. generate children from top 50%
-    4. combine children and parent to make new population
-    5. repeat 2-4 until convergence
-    '''
     
     population = [np.random.uniform(param_lower_bound, param_upper_bound) for _ in range(population_size)]
     losses = [0 for _ in range(population_size)]
     
-    # do weight first
+    # optimize weight first
     for _ in range(generations):
         for i, weight in enumerate(population):
             y_pred = x * weight
@@ -45,7 +34,7 @@ def optimize_weight_and_bias_seperately(x, y):
     best_preds = x * best_weight
 
     
-    # do bias second
+    # optimize bias second
     population = [np.random.uniform(param_lower_bound, param_upper_bound) for _ in range(population_size)]
     
     for _ in range(generations):
@@ -63,5 +52,7 @@ def optimize_weight_and_bias_seperately(x, y):
         population = top_50_percent_of_population + children
     
     best_bias = population[0]
+    
+    return (best_weight, best_bias)
     
     
--- tests/ga_param_optimizer_test.py
+++ tests/ga_param_optimizer_test.py
@@ -1,3 +1,5 @@
+from src.machine_learn.imports import plt
+from src.machine_learn.metrics import r_squared
 from src.machine_learn.data_prep import salary_x, salary_y
 from src.machine_learn.data_manipulation import train_test_validate_split
 
@@ -5,4 +7,13 @@ from src.machine_learn.genetic_algorithms import optimize_weight_and_bias_sepera
 
 def test_ga_param_optimizer():
     x_train, y_train, x_test, y_test, x_val, y_val = train_test_validate_split(salary_x, salary_y)
-    optimize_weight_and_bias_seperately(x_train, y_train)
+    weight, bias = optimize_weight_and_bias_seperately(x_train, y_train)
+    
+    y_pred = (x_val * weight) + bias
+    
+    plt.plot(y_pred)
+    plt.plot(y_val)
+    plt.show()
+    
+    return r_squared(y_pred, y_val)
+    
