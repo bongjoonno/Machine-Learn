c46c0f8242ee3e878197b4cd5e7044555ddcf8b5
bongjoonno <zachary.feryate@gmail.com>
2025-12-05 16:18:35 -0800

    seperated functinos for bias and weight

--- src/machine_learn/genetic_algorithms/ga_param_optimizer.py
+++ src/machine_learn/genetic_algorithms/ga_param_optimizer.py
@@ -9,13 +9,10 @@ param_upper_bound = 1_000_000_000
 population_size = 100
 generations = 100
 
-def optimize_weight_and_bias_seperately(x, y):
+def optimize_weight(x, y):
     n = len(x)
-    
-    population = [np.random.uniform(param_lower_bound, param_upper_bound) for _ in range(population_size)]
     losses = [0 for _ in range(population_size)]
     
-    # optimize weight first
     for _ in range(generations):
         for i, weight in enumerate(population):
             y_pred = x * weight
@@ -31,11 +28,12 @@ def optimize_weight_and_bias_seperately(x, y):
         population = top_50_percent_of_population + children
     
     best_weight = population[0]
-    best_preds = x * best_weight
+    return best_weight
 
-    
-    # optimize bias second
+
+def optimize_bias(x, y, best_preds):
     population = [np.random.uniform(param_lower_bound, param_upper_bound) for _ in range(population_size)]
+    losses = [0 for _ in range(population_size)]
     
     for _ in range(generations):
         for i, bias in enumerate(population):
@@ -53,6 +51,4 @@ def optimize_weight_and_bias_seperately(x, y):
     
     best_bias = population[0]
     
-    return (best_weight, best_bias)
-    
-    
+    return best_bias
--- tests/ga_param_optimizer_test.py
+++ tests/ga_param_optimizer_test.py
@@ -2,7 +2,7 @@ from src.machine_learn.imports import plt
 from src.machine_learn.metrics import r_squared
 from src.machine_learn.data_prep import salary_x, salary_y
 from src.machine_learn.data_manipulation import train_test_validate_split, scale_data
-from src.machine_learn.genetic_algorithms import optimize_weight_and_bias_seperately
+from src.machine_learn.genetic_algorithms import optimize_weight, optimize_bias
 
 from src.machine_learn.data_prep import (salary_x, salary_y, salary_cols_to_scale,
                                          student_x, student_y, student_cols_to_scale, 
@@ -13,20 +13,27 @@ data = [(salary_x, salary_y, salary_cols_to_scale), (student_x, student_y, stude
 
 def test_ga_param_optimizer():
     for x, y, cols_to_scale in data:
+        number_of_features = x.shape[1]
         x_train, y_train, x_val, y_val, x_test, y_test = train_test_validate_split(x, y)
         
         x_train, x_val, x_test = scale_data(x_train, x_val, x_test, columns_to_scale=cols_to_scale)
         
         x_train = x_train.to_numpy().flatten()
+        y_train = y_train.to_numpy()
         
-        weight, bias = optimize_weight_and_bias_seperately(x_train, y_train)
-    
+        weight = optimize_weight(x_train, y_train, number_of_features=number_of_features)
+        best_preds = x_train * weight
+        bias = optimize_bias(x, y, best_preds)
+        
         y_pred = (x_train * weight) + bias
         
         print(r_squared(y_pred, y_train))
         
-        plt.plot(y_pred)
+    
         plt.plot(y_train)
+        plt.plot(y_pred)
+        plt.legend(['y_true', 'y_pred'])
         plt.show()
 
-    
+        
+# scaler should be used on y for GA optimizer
