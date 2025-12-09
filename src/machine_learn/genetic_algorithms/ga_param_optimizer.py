0f94eef89e37562b24a9bae18a35bf9859e4db4d
bongjoonno <zachary.feryate@gmail.com>
2025-12-08 20:02:16 -0800

    coded mutation portion

--- src/machine_learn/genetic_algorithms/ga_param_optimizer.py
+++ src/machine_learn/genetic_algorithms/ga_param_optimizer.py
@@ -1,4 +1,4 @@
-from src.machine_learn.imports import np, plt
+from src.machine_learn.imports import np, plt, random
 from src.machine_learn.metrics import mean_squared_error, r_squared
 from src.machine_learn.genetic_algorithms import GeneticAlgorithm
 
@@ -9,6 +9,8 @@ generations = 100
 
 param_lower_bound = -1
 param_upper_bound = abs(param_lower_bound)
+
+sigma_for_mutation = 0.01
 
 def optimize_weight(x, y):
     population = [np.random.uniform(param_lower_bound, param_upper_bound) for _ in range(population_size)]
@@ -54,7 +56,7 @@ def optimize_bias(x, y):
     
     return best_bias
 
-def ga_optimize_params(x, y):
+def ga_optimize_params(x, y, mutate: bool = True):
     number_of_features = x.shape[1]
     population = [np.random.uniform(param_lower_bound, param_upper_bound, number_of_features) for _ in range(population_size)]
         
@@ -80,14 +82,22 @@ def ga_optimize_params(x, y):
         
         for i in range(number_of_features):
             params = []
-            
+                
             for j in range(len(top_50_percent_of_population)):
                 params.append(top_50_percent_of_population[j][i])
             
-            children.append(GeneticAlgorithm.make_offspring(params))
+            param_children = GeneticAlgorithm.make_offspring(params)
+            
+            if mutate:
+                for k in range(len(param_children)):
+                    if np.random.random() < 0.05:
+                        param_children[k] += random.gauss(0, sigma = sigma_for_mutation)
+            
+            children.append(param_children)
+                    
+                
         
-        children = np.array(children)
-        children = children.T
+        children = np.array(children).T
         children = children.tolist()
         
         population = top_50_percent_of_population + children
