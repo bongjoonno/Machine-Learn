4543540d98c4ddcc7f2b49f01a8271d10cfe1e30
bongjoonno <zachary.feryate@gmail.com>
2025-12-14 04:57:34 -0800

    energy data has been cleaned

--- src/machine_learn/data_prep/energy_usage_data.py
+++ src/machine_learn/data_prep/energy_usage_data.py
@@ -3,4 +3,8 @@ from src.machine_learn.constants import DATA_DIRECTORY
 
 energy_df = pd.read_csv(DATA_DIRECTORY / 'energy_usage.csv')
 
-print(energy_df)
+energy_df = pd.get_dummies(energy_df, columns=['Building Type'], dtype=int)
+energy_df['Day of Week'] = energy_df['Day of Week'].map({'Weekday' : 0, 'Weekend' : 1})
+
+energy_x = energy_df.drop(columns=['Energy Consumption'])
+energy_y = energy_df['Energy Consumption']
