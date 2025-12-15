ded51e33ceacf4694781abb5909b8f29093ada43
bongjoonno <zachary.feryate@gmail.com>
2025-12-14 20:55:45 -0800

    refactored test_ga_lr_optimizer to use template

--- tests/linear_regression_test.py
+++ tests/linear_regression_test.py
@@ -1,6 +1,6 @@
 from src.machine_learn.models import LinearRegression
 from .linear_regression_testing_template import linear_regression_test_template
 
-def test_linear_regression() -> float:
+def test_linear_regression() -> None:
     optimizer = LinearRegression()
-    linear_regression_test_template(optimizer, {}, optimize_lr=True, early_stop=True)
+    linear_regression_test_template(optimizer, {}, optimize_lr=False, early_stop=True)
