2a729a1569d42c38020ea182bc20222d2f5fcb15
bongjoonno <zachary.feryate@gmail.com>
2025-12-14 19:46:55 -0800

    unpack training args into train func

--- tests/linear_regression_testing_template.py
+++ tests/linear_regression_testing_template.py
@@ -5,7 +5,7 @@ from src.machine_learn.data_manipulation import train_test_split, scale_data
 from src.machine_learn.genetic_algorithms import GAOptimizer
 from src.machine_learn.models import LinearRegression
 
-def linear_regression_test_template(optimizer: GAOptimizer | LinearRegression, y_scale: bool = False):
+def linear_regression_test_template(optimizer: GAOptimizer | LinearRegression, training_args: tuple, y_scale: bool = False):
     scaler = StandardScaler()
         
     for x, y, cols_to_scale in linear_regression_test_data:
@@ -16,7 +16,7 @@ def linear_regression_test_template(optimizer: GAOptimizer | LinearRegression, y
             y_train = scaler.fit_transform(y_train.to_numpy().reshape(-1, 1)).flatten()
             y_val = scaler.transform(y_val.to_numpy().reshape(-1, 1)).flatten()
         
-        optimizer.train(x_train, y_train, mutate=False)
+        optimizer.train(x_train, y_train, *training_args)
 
         y_pred = optimizer.predict(x_val)
         
