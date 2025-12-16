418216110d3b54b59567e8675dcc2fe494f04561
bongjoonno <zachary.feryate@gmail.com>
2025-12-16 00:13:00 -0800

    there was a space in fine aggregate column

--- src/machine_learn/data_prep/concrete_data.py
+++ src/machine_learn/data_prep/concrete_data.py
@@ -3,7 +3,7 @@ from src.machine_learn.constants import DATA_DIRECTORY
 
 concrete_df = pd.read_csv(DATA_DIRECTORY / 'concrete.csv')
 
-concrete_columns_to_scale = ['cement', 'blast_furnace', 'fly_ash', 'water', 'superplasticizer', 'coarse_aggregate', 'fine_aggregate', 'age']
+concrete_columns_to_scale = ['cement', 'blast_furnace_slag', 'fly_ash', 'water', 'superplasticizer', 'coarse_aggregate', 'fine_aggregate ', 'age']
 
 concrete_x = concrete_df.drop(columns=['concrete_compressive_strength'])
 concrete_y = concrete_df['concrete_compressive_strength']
