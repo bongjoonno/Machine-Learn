01555f9d29166a673965827785cc9dc056ea21ea
bongjoonno <zachary.feryate@gmail.com>
2025-12-15 23:39:24 -0800

    made non linear test its own function

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
-from tests import test_linear_regression, test_all_models, test_ga_param_optimizer, test_ga_lr_optimizer, test_scikit_learn_linear_regression
+from tests import (test_linear_regression, 
+                  test_all_models, 
+                  test_ga_param_optimizer, 
+                  test_ga_lr_optimizer, 
+                  test_scikit_learn_linear_regression,
+                  test_nonlinear_ga_param_optimizer)
 
 def main():
-    test_ga_param_optimizer()
+    test_nonlinear_ga_param_optimizer()
     #test_scikit_learn_linear_regression()
 
 if __name__ == '__main__':
---
+++ tests/ga_nonlinear_param_optimizer_test.py
@@ -0,0 +1,6 @@
+from src.machine_learn.genetic_algorithms import GANONLinearOptimizer
+from .model_testing_template import model_test_template
+
+def test_nonlinear_ga_param_optimizer() -> None:
+        ga_optimizer = GANONLinearOptimizer()
+        model_test_template(ga_optimizer, {'non_linearity' : True}, early_stop=True, scale_y=True)
