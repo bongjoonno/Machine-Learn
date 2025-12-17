23c23fecb6c23a72c596a8d9933cb067e329d646
zachary <zachary.feryate@gmail.com>
2025-12-17 05:51:36 -0800

    changed wine output to series for propper conformance with tab_pfn

--- src/machine_learn/data_prep/wine_data.py
+++ src/machine_learn/data_prep/wine_data.py
@@ -3,6 +3,6 @@ from src.machine_learn.imports import fetch_ucirepo
 wine_quality = fetch_ucirepo(id=186) 
 
 wine_x = wine_quality.data.features 
-wine_y = wine_quality.data.targets 
+wine_y = wine_quality.data.targets['quality']
   
 wine_columns_to_scale = list(wine_x.columns)
