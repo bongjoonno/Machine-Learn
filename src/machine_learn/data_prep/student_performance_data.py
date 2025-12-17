edcea97a8c6c989108d90a455f01058a2e11862a
zachary <zachary.feryate@gmail.com>
2025-12-17 04:12:05 -0800

    added baseline nonone hot encoded features for aseline models

--- src/machine_learn/data_prep/student_performance_data.py
+++ src/machine_learn/data_prep/student_performance_data.py
@@ -4,9 +4,11 @@ from src.machine_learn.constants import DATA_DIRECTORY
 student_df = pd.read_csv(DATA_DIRECTORY / 'student_performance.csv')
 
 student_df = student_df.dropna()
-student_df['Extracurricular Activities'] = student_df['Extracurricular Activities'].astype('category').cat.codes
-
 student_x = student_df.drop(columns=['Performance Index'])
 student_y = student_df['Performance Index']
 
+student_x_base_line = student_x.copy()
+student_y_base_line = student_y.copy()
+
+student_x['Extracurricular Activities'] = student_x['Extracurricular Activities'].astype('category').cat.codes
 student_cols_to_scale = ['Hours Studied', 'Previous Scores', 'Sleep Hours', 'Sample Question Papers Practiced']
