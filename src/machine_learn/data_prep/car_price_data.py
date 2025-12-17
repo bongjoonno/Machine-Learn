ab36fbb74b54f561096082a53271dfe19babe02e
zachary <zachary.feryate@gmail.com>
2025-12-17 04:16:05 -0800

    made car price non one hot encoded for base line models

--- src/machine_learn/data_prep/car_price_data.py
+++ src/machine_learn/data_prep/car_price_data.py
@@ -13,13 +13,19 @@ car_price_df['CarName'] = car_price_df['CarName'].str.replace('porcshce', 'porsc
 car_price_df['CarName'] = car_price_df['CarName'].str.replace('toyouta', 'toyota')
 car_price_df['CarName'] = car_price_df['CarName'].str.replace('vokswagen', 'volkswagen').str.replace('vw', 'volkswagen')
 
+car_price_x = car_price_df.drop(columns = ['price'])
+car_price_y = car_price_df['price']
+
+car_price_x_base_line = car_price_x.copy()
+car_price_y_base_line = car_price_y.copy()
+
 binary_columns = ['fueltype', 'aspiration', 'doornumber', 'enginelocation']
 multi_columns = ['CarName', 'carbody', 'drivewheel', 'enginetype', 'cylindernumber', 'fuelsystem']
 
+for col in binary_columns+multi_columns:
+    car_price_x_base_line = car_price_x_base_line[col].astype('category').cat.codes
+
 for col in binary_columns:
-    car_price_df[col] = car_price_df[col].astype('category').cat.codes
+    car_price_x[col] = car_price_x[col].astype('category').cat.codes
 
-car_price_df = pd.get_dummies(car_price_df, columns = multi_columns, dtype = int)
-
-car_price_x = car_price_df.drop(columns = ['price'])
-car_price_y = car_price_df['price']
+car_price_x = pd.get_dummies(car_price_x, columns = multi_columns, dtype = int)
