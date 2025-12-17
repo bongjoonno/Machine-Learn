c70683c583ba24f2d8e1eab9266338d4224c148e
zachary <zachary.feryate@gmail.com>
2025-12-17 02:49:40 -0800

    just trying default settings

--- tests/scikit_learn_non_linear_test.py
+++ tests/scikit_learn_non_linear_test.py
@@ -2,7 +2,6 @@ from .scikit_learn_testing_template import test_scikit_learn_baseline
 from src.machine_learn.imports import MLPRegressor
 
 def test_scikit_learn_non_linear():
-    hidden_layers = tuple(100 for _ in range(4))
-    optimizer = MLPRegressor(hidden_layer_sizes=hidden_layers, activation='relu', max_iter=100_000)
+    optimizer = MLPRegressor(max_iter=100_000)
 
     test_scikit_learn_baseline(optimizer, linear_data = False)
