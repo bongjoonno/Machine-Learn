9664b592d0147d04bb55d8ee990d5c3347f1297e
zachary <zachary.feryate@gmail.com>
2025-12-17 02:34:32 -0800

    wrong data was being used to test

--- tests/scikit_learn_testing_template.py
+++ tests/scikit_learn_testing_template.py
@@ -1,14 +1,17 @@
 from src.machine_learn.imports import StandardScaler, LinearRegression, MLPRegressor, plt
-from src.machine_learn.data_prep import linear_regression_test_data
+from src.machine_learn.data_prep import linear_regression_test_data, non_linear_test_data
 from src.machine_learn.data_manipulation import train_test_split, scale_data
 from src.machine_learn.metrics import r_squared
 
 def test_scikit_learn_baseline(optimizer: LinearRegression | MLPRegressor,
-                               scale_y: bool = False, training_args: dict = {}):
+                               training_args: dict = {},
+                               linear_data: bool = True,
+                               scale_y: bool = False):
     scaler = StandardScaler()
+    
+    test_data = linear_regression_test_data if linear_data else non_linear_test_data
         
-        
-    for x, y, cols_to_scale in linear_regression_test_data:
+    for x, y, cols_to_scale in test_data:
         x_train, y_train, x_val, y_val = train_test_split(x, y)
         x_train, x_val = scale_data(x_train, x_val, columns_to_scale=cols_to_scale)
             
