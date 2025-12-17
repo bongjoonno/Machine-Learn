8f2b7e26153ff66a354c26d8da4e7f0b234a7ee7
zachary <zachary.feryate@gmail.com>
2025-12-17 04:18:55 -0800

    got rid of baseline for students, unnecessary data copying

--- src/machine_learn/data_prep/__init__.py
+++ src/machine_learn/data_prep/__init__.py
@@ -4,7 +4,7 @@ from .titanic_data import titanic_x, titanic_y
 
 from .insurance_costs_data import insurance_x, insurance_y, insurance_cols_to_scale
 from .salary_data import salary_x, salary_y, salary_cols_to_scale
-from .student_performance_data import student_x, student_y, student_x_base_line, student_y_base_line, student_cols_to_scale
+from .student_performance_data import student_x, student_y, student_cols_to_scale
 from .car_price_data import car_price_x, car_price_y, car_price_x_base_line, car_price_y_base_line, car_price_cols_to_scale
 from .energy_usage_data import energy_x, energy_y, energy_cols_to_scale
 from .concrete_data import concrete_df, concrete_x, concrete_y, concrete_columns_to_scale
--- src/machine_learn/data_prep/student_performance_data.py
+++ src/machine_learn/data_prep/student_performance_data.py
@@ -7,8 +7,5 @@ student_df = student_df.dropna()
 student_x = student_df.drop(columns=['Performance Index'])
 student_y = student_df['Performance Index']
 
-student_x_base_line = student_x.copy()
-student_y_base_line = student_y.copy()
-
 student_x['Extracurricular Activities'] = student_x['Extracurricular Activities'].astype('category').cat.codes
 student_cols_to_scale = ['Hours Studied', 'Previous Scores', 'Sleep Hours', 'Sample Question Papers Practiced']
