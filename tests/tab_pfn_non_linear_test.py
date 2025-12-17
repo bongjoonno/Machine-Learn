1e7f902fe11d7a45358f7498ffcb107b13199c5c
zachary <zachary.feryate@gmail.com>
2025-12-17 04:05:45 -0800

    tabpfn performs worse on concrete data

--- tests/tab_pfn_non_linear_test.py
+++ tests/tab_pfn_non_linear_test.py
@@ -1,7 +1,9 @@
 from .baseline_testing_template import test_baseline_models
 from src.machine_learn.imports import TabPFNRegressor
 
-def test_tab_pfn_non_linear():
-    optimizer = TabPFNRegressor()
+model_path = r'C:\main\code\repos\Machine-Learn\baseline_models\tabpfn-v2.5-regressor-v2.5_default.ckpt'
 
-    test_baseline_models(optimizer, linear_data = False)
+optimizer = TabPFNRegressor(model_path=model_path, device="cpu", ignore_pretraining_limits=True) 
+
+def test_tab_pfn_non_linear():
+    test_baseline_models(optimizer, linear_data = True)
