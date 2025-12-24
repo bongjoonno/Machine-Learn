88b1b94a2908a36c83a99f8fea6eeb2b5b508bcc
bongjoonno <zachary.feryate@gmail.com>
2025-12-24 11:57:42 -0800

    added next to-do

--- main.py
+++ main.py
@@ -14,9 +14,7 @@ if __name__ == '__main__':
     print(main())
     
 #TO-DO
-# Make everything use PyTorch, speed up from numpy
 # Speed up tests with multi-processesing
-# Make Kfold cross validation code
 # Make template general linear models
 # Get more Linear Regression test data-sets
 # Get more Non-Linear test data-sets
@@ -24,3 +22,4 @@ if __name__ == '__main__':
 # Allow GA param optimizer to use multiple cost-functions (default should be MSE)
 # Fix Logistic Regression Cost function
 # possibly add a PER FEATURE parameter for whether y_pred is computed as func(w*x) or w*func(x) or some other configuration
+#make function crossover method
