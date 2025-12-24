bc299c71d4d0bd0ab3bc8d970bf8a406562aa5e4
bongjoonno <zachary.feryate@gmail.com>
2025-12-24 11:52:53 -0800

    test new saving feature

--- tests/ga_nonlinear_param_optimizer_test.py
+++ tests/ga_nonlinear_param_optimizer_test.py
@@ -1,6 +1,6 @@
 from src.machine_learn.genetic_algorithms import GANONLinearOptimizer
 from .model_testing_template import model_test_template
 
-def test_nonlinear_ga_param_optimizer() -> None:
+def test_nonlinear_ga_param_optimizer(crossover_method: str) -> None:
         ga_optimizer = GANONLinearOptimizer()
-        return model_test_template(ga_optimizer, training_args={'non_linearity' : True, 'crossover_method' : 'sbx'}, early_stop=True, scale_y=True)
+        return model_test_template(ga_optimizer, training_args={'non_linearity' : True, 'crossover_method' : crossover_method}, early_stop=True, scale_y=True)
