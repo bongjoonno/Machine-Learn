2b3e4a2baeb253c5947ad2bf84e4c376e153d7ac
zachary <zachary.feryate@gmail.com>
2026-01-23 22:02:41 -0800

    changed data to be scaled in data prep stage not algorithm stage

--- src/machine_learn/data_prep/california_housing_data.py
+++ src/machine_learn/data_prep/california_housing_data.py
@@ -1,16 +1,17 @@
 from src.machine_learn.imports import pd
 from src.machine_learn.constants import DATA_DIRECTORY
-from src.machine_learn.data_manipulation import split_k_folds
+from src.machine_learn.data_manipulation import split_k_folds, scale_data
 
 california_housing_df = pd.read_csv(DATA_DIRECTORY / 'california_housing.csv')
 california_housing_df = california_housing_df.sample(frac=1, random_state=42)
 
-california_housing_df['ocean_proximity'] = california_housing_df['ocean_proximity'].astype('category').cat.codes
+california_housing_df = pd.get_dummies(california_housing_df, columns='ocean_proximity', dtype=int)
 
 california_housing_x = california_housing_df.drop(columns='median_house_value')
 
 california_housing_y = california_housing_df['median_house_value']
-california_housing_cols_to_scale = list(california_housing_x.columns)
+cols_to_scale = ['longitude', 'latitude', 'housing_median_age', 'total_rooms',
+                                    'total_bedrooms', 'population', 'households', 'median_income', 'median_house_value']
 
 
 california_housing_data = split_k_folds(california_housing_x, california_housing_y, k=1)
@@ -18,4 +19,5 @@ california_housing_data = split_k_folds(california_housing_x, california_housing
 for data in california_housing_data:
     california_housing_data[0]['total_bedrooms'] = california_housing_data[0]['total_bedrooms'].fillna(california_housing_data[0]['total_bedrooms'].median())
     california_housing_data[2]['total_bedrooms'] = california_housing_data[2]['total_bedrooms'].fillna(california_housing_data[0]['total_bedrooms'].median())
-    
+
+    california_housing_data[0], california_housing_data[2] = scale_data(california_housing_data[0], california_housing_data[2], cols_to_scale)    
--- tests/model_testing_template.py
+++ tests/model_testing_template.py
@@ -13,59 +13,23 @@ def model_test_template(optimizer: LinearRegression | GAOptimizer | GANONLinearO
                         optimize_lr: bool = False, 
                         scale_y: bool = False):
     
-    n = 1
-    
-    avg_r2s = []
-    
-    all_data_folds = [split_k_folds(x, y, n) for x, y, _ in regression_test_data]
-    all_cols_to_scale = [cols_to_scale for _, _, cols_to_scale in regression_test_data]
-    
-    training_packages = [(fold, cols_to_scale, optimizer, training_args, early_stop, optimize_lr, scale_y) 
-                         for fold, cols_to_scale in zip(all_data_folds, all_cols_to_scale)]
-    
-    
-    avg_r2s = [k_cross_validation_train(*args) for args in training_packages]
-    
-    return avg_r2s
+    r2s = []
+
+    for data in regression_test_data:
+        x_train, y_train, x_val, y_val = data*
         
+        if optimize_lr:
+            lr_optimizer = GAlrOptimizer(optimizer, x_train, y_train, x_val, y_val)
+            lr = lr_optimizer.optimize_lr()
+            training_args['learning_rate'] = lr
+            
+        if early_stop:
+            optimizer.train(x_train, y_train, x_val, y_val, **training_args)
+        else:
+            optimizer.train(x_train, y_train, **training_args)
+
+        y_pred = optimizer.predict(x_val)
         
-
-def k_cross_validation_train(*args):
-    folds, cols_to_scale, optimizer, training_args, early_stop, optimize_lr, scale_y = args
-
-    args_packages = [(fold, cols_to_scale, optimizer, training_args, early_stop, optimize_lr, scale_y)
-                     for fold in folds]
+        r2s.append(r_squared(y_pred, y_val))
     
-   
-    r2s = [fold_train(*args) for args in args_packages]
-    print(r2s)
-    return np.mean(r2s)
-
-    
-    
-def fold_train(*args):
-    fold, cols_to_scale, optimizer, training_args, early_stop, optimize_lr, scale_y = args
-    
-    x_train, y_train, x_val, y_val = fold
-    
-    x_train, x_val = scale_data(x_train, x_val, columns_to_scale=cols_to_scale)
-        
-    if scale_y:
-        y_train = scaler.fit_transform(y_train.to_numpy().reshape(-1, 1)).flatten()
-        y_val = scaler.transform(y_val.to_numpy().reshape(-1, 1)).flatten()
-    
-    if optimize_lr:
-        lr_optimizer = GAlrOptimizer(optimizer, x_train, y_train, x_val, y_val)
-        lr = lr_optimizer.optimize_lr()
-        training_args['learning_rate'] = lr
-        
-    if early_stop:
-        optimizer.train(x_train, y_train, x_val, y_val, **training_args)
-    else:
-        optimizer.train(x_train, y_train, **training_args)
-
-    y_pred = optimizer.predict(x_val)
-    
-    return r_squared(y_pred, y_val)
-
-    
+    return r2s
