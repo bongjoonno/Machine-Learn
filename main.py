dc034fa0277d3402f9971f27409900939bc8063c
zachary <zachary.feryate@gmail.com>
2026-01-23 21:02:43 -0800

    mae cost function works

--- main.py
+++ main.py
@@ -3,15 +3,16 @@ from tests import (test_tab_pfn_non_linear,
 
 from src.machine_learn.imports import np, Path
 
-save_path = Path(r'D:\code\repos\Machine-Learn\numpy_data')
+save_path_desktop = Path(r'D:\code\repos\Machine-Learn\numpy_data')
+save_path_laptop = Path(r'C:\main\code\repos\Machine-Learn\numpy_data')
 
 def main():
     #np.save(save_path / 'tab_pfn_regression', test_tab_pfn_non_linear())
     #np.save(save_path / 'GANON_regression_sbx', test_nonlinear_ga_param_optimizer(crossover_method='sbx'))
     #np.save(save_path / 'GANON_regression_arithmetic_crossover', test_nonlinear_ga_param_optimizer(crossover_method='arithmetic'))
     #np.save(save_path / 'GANON_regression_arithmetic_crossover_sympy_2', test_nonlinear_ga_param_optimizer(crossover_method='arithmetic'))
-    np.save(save_path / 'GANON_regression_sbx_function_crossover_sympy', test_nonlinear_ga_param_optimizer({'non_linearity' : True, 
-                                                                                                            'cost_function' : 'mpd',
+    np.save(save_path_laptop / 'GANON_regression_sbx_function_crossover_sympy', test_nonlinear_ga_param_optimizer({'non_linearity' : True, 
+                                                                                                            'cost_function' : 'mae',
                                                                                                             'selection_method' : 'rank', 
                                                                                                             'crossover_method' : 'sbx', 
                                                                                                             'function_crossover_method' : 'arithmetic'}))
